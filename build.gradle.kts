fc42ec61bb2aeb7dcb7d50dba97d7328b182e59d
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2025-04-10 17:32:26 +0200

    Remove redundant import statements in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,6 @@ import buildutils.configureDetekt
 import buildutils.createDetektTask
 import buildutils.configureDiktat
 import buildutils.createDiktatTask
-import buildutils.configureDetekt
-import buildutils.createDetektTask
-import buildutils.configureDiktat
-import buildutils.createDiktatTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 @Suppress("DSL_SCOPE_VIOLATION") // "libs" produces a false-positive warning, see https://youtrack.jetbrains.com/issue/KTIJ-19369
